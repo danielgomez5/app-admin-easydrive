829ad641c43aede45a50cead8acf053d4870d7c2
dgomezperez4 <daniel_gomezperez@iescarlesvallbona.cat>
2025-03-28 17:28:57 +0100

    disseny formulari principal modificat

--- AppAdminEasyDrive/View/Form1.Designer.cs
+++ AppAdminEasyDrive/View/Form1.Designer.cs
@@ -20,7 +20,7 @@ namespace AppAdminEasyDrive
             // 
             // panelCabecera
             // 
-            this.panelCabecera.BackColor = System.Drawing.Color.LightGray;
+            this.panelCabecera.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
             this.panelCabecera.Controls.Add(this.btnDashboard);
             this.panelCabecera.Controls.Add(this.btnConductors);
             this.panelCabecera.Controls.Add(this.btnUsuaris);
@@ -37,6 +37,8 @@ namespace AppAdminEasyDrive
             // 
             this.btnDashboard.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDashboard.BackgroundImage")));
             this.btnDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDashboard.Font = new System.Drawing.Font("DejaVu Sans Condensed", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDashboard.Location = new System.Drawing.Point(12, 12);
             this.btnDashboard.Name = "btnDashboard";
@@ -45,43 +47,59 @@ namespace AppAdminEasyDrive
             // 
             // btnConductors
             // 
+            this.btnConductors.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(151)))), ((int)(((byte)(240)))));
             this.btnConductors.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConductors.FlatAppearance.BorderSize = 0;
+            this.btnConductors.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConductors.Font = new System.Drawing.Font("DejaVu Sans Condensed", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnConductors.Location = new System.Drawing.Point(443, 12);
             this.btnConductors.Name = "btnConductors";
             this.btnConductors.Size = new System.Drawing.Size(255, 58);
             this.btnConductors.TabIndex = 0;
             this.btnConductors.Text = "Conductors";
+            this.btnConductors.UseVisualStyleBackColor = false;
             // 
             // btnUsuaris
             // 
+            this.btnUsuaris.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(151)))), ((int)(((byte)(240)))));
             this.btnUsuaris.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUsuaris.FlatAppearance.BorderSize = 0;
+            this.btnUsuaris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUsuaris.Font = new System.Drawing.Font("DejaVu Sans Condensed", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.btnUsuaris.Location = new System.Drawing.Point(728, 12);
             this.btnUsuaris.Name = "btnUsuaris";
             this.btnUsuaris.Size = new System.Drawing.Size(255, 58);
             this.btnUsuaris.TabIndex = 1;
             this.btnUsuaris.Text = "Usuaris";
+            this.btnUsuaris.UseVisualStyleBackColor = false;
             // 
             // btnCoches
             // 
+            this.btnCoches.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(151)))), ((int)(((byte)(240)))));
             this.btnCoches.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCoches.FlatAppearance.BorderSize = 0;
+            this.btnCoches.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCoches.Font = new System.Drawing.Font("DejaVu Sans Condensed", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.btnCoches.Location = new System.Drawing.Point(443, 93);
             this.btnCoches.Name = "btnCoches";
             this.btnCoches.Size = new System.Drawing.Size(255, 58);
             this.btnCoches.TabIndex = 2;
             this.btnCoches.Text = "Cotxes";
+            this.btnCoches.UseVisualStyleBackColor = false;
             // 
             // btnZones
             // 
+            this.btnZones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(151)))), ((int)(((byte)(240)))));
             this.btnZones.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnZones.FlatAppearance.BorderSize = 0;
+            this.btnZones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnZones.Font = new System.Drawing.Font("DejaVu Sans Condensed", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.btnZones.Location = new System.Drawing.Point(728, 93);
             this.btnZones.Name = "btnZones";
             this.btnZones.Size = new System.Drawing.Size(255, 58);
             this.btnZones.TabIndex = 3;
             this.btnZones.Text = "Zones";
+            this.btnZones.UseVisualStyleBackColor = false;
             // 
             // panelContenido
             // 
